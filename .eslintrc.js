af687e7231016d34bb49a603946d84a352bb6092
icaroalmeida-somma <icaro.almeida@somma-it.com>
2023-10-21 23:41:15 -0300

    Início do projeto de apirest

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  plugins: ['prettier', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['**/*.ts'],
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+    },
+    {
+      files: ['knexfile.ts'],
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+    },
+    {
+      files: ['migrations/*.ts'],
+      parserOptions: {
+        project: 'tsconfig.json',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  ignorePatterns: ['node_modules/', '_explicacoes/'],
+  rules: {
+    'no-console': 'error',
+    quotes: ['error', 'single'],
+  },
+};
---
+++ db/migrations/20231022021119_create-document.ts
@@ -0,0 +1,7 @@
+import { Knex } from 'knex';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export async function up(knex: Knex): Promise<void> {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export async function down(knex: Knex): Promise<void> {}
