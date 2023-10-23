71317679058605c52e269e2f5c759317e363b926
icaroalmeida-somma <icaro.almeida@somma-it.com>
2023-10-22 22:38:04 -0300

    Criação de migrations com knex

--- db/migrations/20231022021119_create-document.ts
+++
@@ -1,7 +0,0 @@
-import { Knex } from 'knex';
-
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export async function up(knex: Knex): Promise<void> {}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-export async function down(knex: Knex): Promise<void> {}
