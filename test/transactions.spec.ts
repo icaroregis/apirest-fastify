56b66ee8d3f2828859fb98ebe4344d5c97950142
icaroalmeida-somma <icaro.almeida@somma-it.com>
2023-11-09 23:08:59 -0300

    feate: Deploy da aplicação

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,7 +1,7 @@
 import { test, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest';
 import { execSync } from 'node:child_process';
-import request from 'supertest';
 import { app } from '../src/app';
+import request from 'supertest';
 
 // * Os testes são compostos de tres partes:
 // * O título.
